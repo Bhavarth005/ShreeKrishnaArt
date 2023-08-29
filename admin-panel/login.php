7adb62efea1bde6d1f5d0580a9b452c114b3bb56
Bhavarth005 <abcbhavarth@gmail.com>
2023-08-29 11:31:06 +0530

    added pulled files

---
+++ admin-panel/login.php
@@ -0,0 +1,46 @@
+<?php
+    $err = false;
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $password_hashed = md5($password);
+
+        include ("../globals.php");
+
+        $result = mysqli_query($con, "SELECT * FROM $users WHERE username='$username' AND password='$password_hashed'");
+
+        if(mysqli_num_rows($result)){
+            session_start();
+            $_SESSION["login"] = true;
+            header("location: index.php");
+        }else{
+            $err = true;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="../css/admin-login.css">
+</head>
+<body>
+    <div class="main-container">
+        <div class="logo-container">
+            <img src="../logo/gold.png" class="logo">
+            <h1>Shree Krishna Art</h1>
+        </div>
+        <h1 class="heading">Login to Admin Panel</h1>
+        <form action="./admin-home.php" method="POST" class="form">
+            <p class="field-label">Username</p>
+            <input type="text" name="username" id="username" placeholder="Enter Username" required>
+            <p class="field-label label-pw">Password</p>
+            <input type="password" name="password" id="password" placeholder="Enter Password" required>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
